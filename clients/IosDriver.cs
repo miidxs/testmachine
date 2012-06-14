de3ad14049b4c0f104030f811070d382d9e0de0a
Peter Farkas <peter.farkas@testmachine.ch>
2012-06-14 11:56:26 +0200

    documented C# client

--- clients/IosDriver.cs
+++ clients/IosDriver.cs
@@ -7,7 +7,7 @@ using CookComputing.XmlRpc;
 
 public class IPhoneDriver {
 
-    public interface IXpathDriver : IXmlRpcProxy {
+	interface IXpathDriver : IXmlRpcProxy {
 
         [XmlRpcMethod("tap")]
         bool tap(String xpath);
@@ -20,6 +20,9 @@ public class IPhoneDriver {
 
         [XmlRpcMethod("setText")]
         bool setText(String xpath, String text);
+
+        [XmlRpcMethod("setSwitch")]
+        bool setSwitch(String xpath, bool on);
 
         [XmlRpcMethod("selectPickerViewRow")]
         bool selectPickerViewRow(String xpath, int rowIndex, int componentIndex);
@@ -64,20 +67,30 @@ public class IPhoneDriver {
 
     private DateTime _startTime;
 
-   public IPhoneDriver(String deviceIp) {
+    public IPhoneDriver(String deviceIp) {
         _deviceIp = deviceIp;
         _client = XmlRpcProxyGen.Create<IXpathDriver>();
         SetPort(DEFAULT_PORT);
 	}
 
+	/// <summary>
+	/// Sets the port. Every app runs its own server on its own port. 
+	/// </summary> 
     public void SetPort(int port) {
         _client.Url = "http://" + _deviceIp + ":" + port + "/rpc";
     }
 
+	/// <summary>
+	/// Sets the amount of the time, operations on UI elements should wait for the element to appear.
+	/// </summary> 
     public void SetTimeOut(double timeOut) {
         _timeOut = timeOut;
     }
 
+	/// <summary>
+	/// Taps the element specified by the XPath expression at its center.
+	/// <exception cref="System.Exception">Thrown when the element didn't appear within timeout or the operation failed</exception>
+	/// </summary> 
     public void Tap(String xpath) {
         Reset();
         while (!_client.tap(xpath)) {
@@ -85,6 +98,10 @@ public class IPhoneDriver {
         }
     }
 
+	/// <summary>
+	/// Taps the element specified by the XPath expression at the specified coordinates.
+	/// <exception cref="System.Exception">Thrown when the element didn't appear within timeout or the operation failed</exception>
+	/// </summary> 
     public void Tap(String xpath, double x, double y) {
         Reset();
         while (!_client.tap(xpath, x, y)) {
@@ -92,6 +109,10 @@ public class IPhoneDriver {
         }
     }
 
+	/// <summary>
+	/// Performs a swipe on the element specified by the XPath expression, if the element is present.
+	/// <exception cref="System.Exception">Thrown when the element didn't appear within timeout or the operation failed</exception>
+	/// </summary> 
     public void Swipe(String xpath, double x1, double y1, double x2, double y2) {
         Reset();
         while (!_client.swipe(xpath, x1, y1, x2, y2)) {
@@ -99,6 +120,10 @@ public class IPhoneDriver {
         }
     }
 
+	/// <summary>
+	/// Sets the text value of the element specified by the XPath expression.
+	/// <exception cref="System.Exception">Thrown when the element didn't appear within timeout or the operation failed</exception>
+	/// </summary> 
     public void SetText(String xpath, String text) {
         Reset();
         while (!_client.setText(xpath, text)) {
@@ -106,6 +131,21 @@ public class IPhoneDriver {
         }
     }
 
+	/// <summary>
+	/// Sets the switch specified by the XPath expression on or off, if the element is present.
+	/// <exception cref="System.Exception">Thrown when the element didn't appear within timeout or the operation failed</exception>
+	/// </summary> 
+    public void SetSwitch(String xpath, bool on) {
+        Reset();
+        while (!_client.setSwitch(xpath, on)) {
+            FailOrWait();
+        }
+    }
+
+	/// <summary>
+	/// Sets a component of the picker view specified by the XPath expression, if the element is present.
+	/// <exception cref="System.Exception">Thrown when the element didn't appear within timeout or the operation failed</exception>
+	/// </summary> 
     public void SelectPickerViewRow(String xpath, int rowIndex, int componentIndex) {
         Reset();
         while (!_client.selectPickerViewRow(xpath, rowIndex, componentIndex)) {
@@ -113,6 +153,10 @@ public class IPhoneDriver {
         }
     }
 
+	/// <summary>
+	/// Sets a component of the picker view specified by the XPath expression, if the element is present.
+	/// <exception cref="System.Exception">Thrown when the element didn't appear within timeout or the operation failed</exception>
+	/// </summary> 
     public void ScrollToTableViewRow(String xpath, int sectionIndex, int rowIndex) {
         Reset();
         while (!_client.scrollToTableViewRow(xpath, sectionIndex, rowIndex)) {
@@ -120,6 +164,10 @@ public class IPhoneDriver {
         }
     }
 
+	/// <summary>
+	/// Checks if the element specified by the XPath expression is present.
+	/// Returns false, if the element was not present, true if it was present.
+	/// </summary>
     public bool Check(String xpath) {
         Reset();
         DateTime now = DateTime.Now;
@@ -132,6 +180,9 @@ public class IPhoneDriver {
         return true;
     }
 
+	/// <summary>
+	/// Starts playing sound.
+	/// </summary>
     public void StartPlayingSound(String soundFilePath) {
         FileStream s = new FileStream(soundFilePath, FileMode.Open);
         BinaryReader r = new BinaryReader(s);
@@ -139,18 +190,31 @@ public class IPhoneDriver {
         _client.startPlayingSound(data);
     }
 
+	/// <summary>
+	/// Pauses playing the sound.
+	/// </summary>
     public void PausePlayingSound() {
         _client.pausePlayingSound();
     }
 
+	/// <summary>
+	/// Resumes playing the sound.
+	/// </summary>
     public void ResumePlayingSound() {
         _client.resumePlayingSound();
     }
 
+	/// <summary>
+	/// Starts recording sound in WAV file.
+	/// </summary>
     public void StartRecordingSound(double sampleRate) {
         _client.startRecordingSound(sampleRate);
     }
 
+	/// <summary>
+	/// Finishes recording sound.
+	/// Return the sound as a raw byte array.
+	/// </summary>
     public void FinishRecordingSound(String soundFilePath) {
         byte[] data = _client.finishRecordingSound();
         FileStream s = new FileStream(soundFilePath, FileMode.Create);
@@ -158,10 +222,19 @@ public class IPhoneDriver {
         s.Close();
     }
 
+	/// <summary>
+	/// Opens another application and jumps to the specified module by calling UIApplication:openURL:.
+	/// The exact URL format is not documented, but examples can be found in the internet.
+	/// </summary>
     public void OpenUrl(String url) {
         _client.openUrl(url);
     }
 
+	/// <summary>
+	/// Calls an SBSettings command after a specified delay.
+	/// The purpose of the delay is to allow the call to return when invoking the reboot command.
+	/// The command must be installed on the phone. See http://stackoverflow.com/questions/6419678/iphone-reboot-programmatically.
+	/// </summary>
     public void NotifyPost(String name, double delay) {
         _client.notifyPost(name, delay);
     }
